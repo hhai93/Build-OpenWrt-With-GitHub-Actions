c2efad29eeec2a138063f11241e52d5c2c71ae82
hhai93 <70931114+hhai93@users.noreply.github.com>
2025-04-23 07:17:41 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+echo 'src-git argon https://github.com/jerrykuku/luci-app-argon-config' >>feeds.conf.default
