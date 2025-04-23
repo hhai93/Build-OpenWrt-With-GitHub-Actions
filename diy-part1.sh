90625c38aa5c0762ad8301d173d23d033a1ee76a
hhai93 <phhai93@gmail.com>
2025-04-23 10:01:19 +0700

    Revert "Update diy-part1.sh"
    This reverts commit c2efad29eeec2a138063f11241e52d5c2c71ae82.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
-echo 'src-git argon https://github.com/jerrykuku/luci-app-argon-config' >>feeds.conf.default
